e10d96a850a35592f89c095250536a66d824b1f0
lucifertrj <jaintarun.abd17@gmail.com>
2021-08-08 13:24:55 +0530

    👷 Straw Hat Pirates Data Profile

---
+++ bio.py
@@ -0,0 +1,83 @@
+#complete data on Straw Hats
+straw_hat={
+    "Monkey D Luffy":{
+    "Role":"Captain",
+    "Nickname":"mugiwara luffy/5th Yonko/",
+    "Devil Fruit":"Gomu Gomu no mi",
+    "Haki":"3/3",
+    "Current Bounty(berries)":"1.5 billion",
+    "Dream":"To Become Pirate King"
+    },
+    "Roronoa Zoro":{
+    "Role":"Vice Captain/Swordsman",
+    "Nickname":"Pirate hunter Roronoa Zoro",
+    "Devil Fruit":"None",
+    "Haki":"2/3",
+    "Current Bounty(berries)":"320 million",
+    "Dream":"To Become Strongest Swordsman"
+    },
+    "Nami":{
+    "Role":"Navigator",
+    "Nickname":"Cat Buglar",
+    "Devil Fruit":"None",
+    "Haki":"0/3",
+    "Current Bounty(berries)":"66 million",
+    "Dream":"To Draw a map of the entire world"
+    },
+    "Usopp":{
+    "Role":"Sniper",
+    "Nickname":"Sogeking/ God Usopp",
+    "Devil Fruit":"None",
+    "Haki":"1/3",
+    "Current Bounty(berries)":"200 million",
+    "Dream":"To become Bravest Warrior"
+    },
+    "Vinsmoke Sanji":{
+    "Role":"Chef",
+    "Nickname":"Black Foot",
+    "Devil Fruit":"None",
+    "Haki":"2/3",
+    "Current Bounty(berries)":"330 million",
+    "Dream":"To visit All blue"
+    },
+    "Tony Tony Chopper":{
+    "Role":"Doctor",
+    "Nickname":"Cotton Candy Lover",
+    "Devil Fruit":"Hito Hito no mi",
+    "Haki":"0/3",
+    "Current Bounty(berries)":"100",
+    "Dream":"To Cure all Disease and become Best Doctor"
+    },
+    "Nico Robin":{
+    "Role":"Archaeologist",
+    "Nickname":"Devil Child",
+    "Devil Fruit":"Hana Hana no mi",
+    "Haki":"0/3",
+    "Current Bounty(berries)":"130 million",
+    "Dream":"To unveil the secret of Poneglyphs"
+    },
+    "Franky":{
+    "Role":"Ship Wright",
+    "Nickname":"Cyborg",
+    "Devil Fruit":"None",
+    "Haki":"0/3",
+    "Current Bounty(berries)":"94 million",
+    "Dream":"Build a dream ship that could reach end of grand line"
+    },
+    "Brook":{
+    "Role":"Musician",
+    "Nickname":"Soul King",
+    "Devil Fruit":"Yomi Yomi no mi",
+    "Haki":"0/3",
+    "Current Bounty(berries)":"83 million",
+    "Dream":"Reunion with Laboon"
+    },
+    "Jimbei":{
+    "Role":"Helmsman",
+    "Nickname":"Knight of the sea",
+    "Devil Fruit":"None",
+    "Haki":"2/3",
+    "Current Bounty(berries)":"438 million",
+    "Dream":"To bring peace between Fishman and Human"
+    }
+    }
